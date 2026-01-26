e635562b8c6bb42dce1070659ff1e740d0053ca7
Arijit Das <arijitdas.das99@gmail.com>
2026-01-27 01:58:46 +0530

    updated

--- Phase 1/StreamlitBasics.py
+++ Phase 1/StreamlitBasics.py
@@ -1,3 +1,12 @@
+"""
+Docstring for Phase 1.StreamlitBasics => Text display
+"""
+
 import streamlit as st
 
-st.title("Streamlit App")
+st.title("title function")
+st.header("header function")
+st.subheader("Subheader function")
+st.write("write function")
+st.text("text function")
+st.caption("caption function")
