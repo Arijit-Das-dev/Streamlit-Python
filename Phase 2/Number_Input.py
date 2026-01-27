b9238ec161e061d7ab45319c9b4b5201e221cd71
Arijit Das <arijitdas.das99@gmail.com>
2026-01-27 19:26:04 +0530

    Updated

--- Phase 2/Number_Input.py
+++ Phase 2/Number_Input.py
@@ -3,18 +3,19 @@ import streamlit as st
 st.number_input(
 
     label="Age",
-    min_value=19,
+    min_value=18,
     max_value=100,
-    help="Age must contain withing 19 to 100",
     disabled=False,
+    help="Age allowed : 18 -> 100",
     step=1,
-    icon="🔞"
+    icon=None
 )
 
 st.slider(
 
-    label="Enter range",
-    min_value=1,
-    step=1,
-    max_value=100
+    label="Batch_size",
+    min_value=18,
+    max_value=100,
+    help="Age allowed : 18 - 100",
+    step=1
 )
