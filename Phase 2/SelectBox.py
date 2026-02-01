cddc689a5bb55d0506a85c3ecee15224b925123b
Arijit Das <arijitdas.das99@gmail.com>
2026-02-01 20:43:51 +0530

    Updated some files and deleted one file

--- Phase 2/SelectBox.py
+++ Phase 2/SelectBox.py
@@ -1,41 +1,27 @@
 import streamlit as st
 
-col1, col2, col3, col4, col5 = st.columns(5)
-
-with col1:
-
-    st.selectbox(
+st.selectbox(
     label="Select below",
+    placeholder="Select the options",
     options=["A", "B", "C"],
     help="Select from the options",
     label_visibility="visible",
     index=None,
 )
-
-with col2:
-
-    st.multiselect(
+st.multiselect(
     label="Select below",
     options=["A", "B", "C", "D"],
     help="select the options",
-    label_visibility="visible",
-    
-) 
+    label_visibility="visible",) 
 
-with col3:
-
-    st.radio(
-
+st.radio(
         label="Select the options below",
         options=["A", "B", "C", "D"],
         label_visibility="visible",
         help="Select from the options",
         index=None
 )
-
-with col4:
-
-    st.checkbox(
+st.checkbox(
     "Accept Terms & Conditions",
     value=False
 )
